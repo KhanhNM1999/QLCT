d63a9b153fffdd2359246852af248693270afe03
ST-KhanhNM26 <khanhnm26@gmail.com>
2026-08-27 20:31:10 +0700

    Deploy selected month payments v9

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "qlct-shell-v8"
+const CACHE_NAME = "qlct-shell-v9"
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=8",
-  "./bank-directory.js?v=8",
-  "./app.js?v=8",
+  "./styles.css?v=9",
+  "./bank-directory.js?v=9",
+  "./app.js?v=9",
   "./manifest.webmanifest",
   "./assets/app-icon-180.png",
   "./assets/app-icon-512.png"
